bc2e20ae9cbbe1acf7c641a252b1664fd092ec39
趙德柱 <w06rup3vu06@gmail.com>
2024-05-25 16:51:58 +0800

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 
 module.exports = {
-  // mongoURI: "mongodb://" + process.env.MONGODB_SERVER + ":" + process.env.MONGODB_PORT + "/" + process.env.MONGODB_DATABASE,
+  mongoURI: "mongodb://" + process.env.MONGODB_SERVER + ":" + process.env.MONGODB_PORT + "/" + process.env.MONGODB_DATABASE,
   // 線下開發用
-  mongoURI: "mongodb://" + process.env.MONGODB_SERVER + ":" + process.env.MONGODB_PORT + "/" + process.env.MONGODB_DATABASE_DEV,
+  // mongoURI: "mongodb://" + process.env.MONGODB_SERVER + ":" + process.env.MONGODB_PORT + "/" + process.env.MONGODB_DATABASE_DEV,
   secretOrKey: process.env.SECRET_OR_KEY,
   rootPassword: process.env.ROOT_PASSWORD,
 };
